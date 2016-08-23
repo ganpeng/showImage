1f0af4f62a60d4f990a4aee67754958614667805
louisGan <464860687@qq.com>
2016-08-23 17:29:51 +0800

    router

--- config/express.js
+++ config/express.js
@@ -1,7 +1,8 @@
 'use strict'
 const compression = require('compression');
 const express = require('express');
-const join = requir('path').join;
+const nunjucks = require('nunjucks');
+const join = require('path').join;
 const config = require('./');
 
 
@@ -12,5 +13,12 @@ module.exports = (app) => {
 
     // 静态资源管理
 
-    app.use(express.static(join(config.root, 'public')));
+    app.use(express.static(join('..', 'public')));
+
+    nunjucks.configure(join(config.root, 'view'), { // 设置模板文件的目录，为views
+        autoescape: true,
+        express: app
+    });
+    app.set('view engine', 'html');
+
 }
