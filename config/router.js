07241c074eec92ee188ec6922aa835b756e7dbef
louisGan <464860687@qq.com>
2016-08-25 11:42:31 +0800

    首页

--- config/router.js
+++ config/router.js
@@ -1,26 +1,13 @@
 'use strict'
+const config = require('./')
+const join = require('path').join;
+const index = require(join(config.root, 'app/controller/index'));
+const user = require(join(config.root, 'app/controller/user'));
+
 
 module.exports = (app) => {
 
-
-    app.get('/', (req, res) => {
-        req.session.name = 'louis';
-
-        res.render('index.html', {
-            name: req.session.name
-        })
-    })
-
-
-    app.get('/to', (req, res) => {
-        let name = req.session.name;
-        res.render('user.html', {
-            name: name
-        })
-    })
-
-    app.get('/dongdong', (req, res) => {
-        res.send('I love dongdong!!!');
-    })
+    app.route('/')
+        .get(index)
 
 }
