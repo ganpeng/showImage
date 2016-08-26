5770633a856951e49a908b287511a4b408c9aa56
louisGan <464860687@qq.com>
2016-08-26 09:36:48 +0800

    删除多余依赖

--- app/model/user.js
+++ app/model/user.js
@@ -1,14 +1,6 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const Promise = require('bluebird');
-
-const genSalt = Promise.promisify(bcrypt.genSalt);
-const hash = Promise.promisify(bcrypt.hash);
-const compare = Promise.promisify(bcrypt.compare);
-
-
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
@@ -36,29 +28,6 @@ const UserSchema = new Schema({
         default : Date.now
     }
 });
-
-
-UserSchema.methods = {
-
-    authenticate : (password) => {
-        this.encryptPasswor(password)
-            .then((hashPass) => {
-                return this.hashedPassword === hashPass;
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    },
-
-    makeSalt : () => {
-        return genSalt(10);
-    },    
-
-    encryptPassword : (password) => {
-        return hash(password, this.salt);
-    }
-}
-
 
 const User = mongoose.model('User', UserSchema);
 
